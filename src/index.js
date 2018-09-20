6f174fc0919a5232c20614329569d74daaed9245
Dan Actor <danltactor@gmail.com>
2018-09-20 11:19:07 -0600

    clean up index.js, add new test case

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const packageInstaller = (d) => {
-  if (!d) { return null }
-  if (!Array.isArray(d)) { throw "Input is not an array" }
+  if (!d) return null
+  if (!Array.isArray(d)) throw "Input is not an array"
 }
 
 module.exports = packageInstaller;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,25 @@
-//const sum = require('../src/sum');
 const packageInstaller = require('../src/index');
 
 describe('Package Installer', () => {
-  it('returns null when given no input', () => {
+  it('should return null if given no input', () => {
     expect(packageInstaller()).toBeNull()
   });
 
-  it('throws error if input is not array', () => {
+  it('should throw error if input is not array', () => {
     expect( () => {
       packageInstaller("packageInstaller: jest")
     }).toThrow("Input is not an array")
   });
+
+  it('should work for a small number of packages', () => {
+    let testInput = [
+      { name: "packageInstaller", dependencies: ["jest"] },
+      { name: "jest" }
+    ]
+    let expected = ["jest", "packageInstaller"]
+
+    expect(packageInstaller(testInput)).toEqual(expected)
+  });
+
 });
 
